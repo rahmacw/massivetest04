4a40d74cdc1d87c4ef5fbcd81edacb8b16cbc873
rahmacw <150320121+rahmacw@users.noreply.github.com>
2024-11-25 22:23:24 +0700

    Update App.jsx

--- gomarina-user/src/App.jsx
+++ gomarina-user/src/App.jsx
@@ -6,20 +6,34 @@ import Home from './pages/Home';
 import About from './pages/About';
 import Gallery from './pages/Gallery';
 import Product from './pages/Product';
+import SignIn from './pages/SignIn';
 
 const App = () => {
   return (
     <BrowserRouter>
-      <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/gallery" element={<Gallery />} />
-        <Route path="/product" element={<Product />} />
+        {/* Halaman Sign In tanpa Header dan Footer */}
+        <Route path="/sign-in" element={<SignIn />} />
+        
+        {/* Rute dengan Header dan Footer */}
+        <Route
+          path="*"
+          element={
+            <>
+              <Header />
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/gallery" element={<Gallery />} />
+                <Route path="/product" element={<Product />} />
+              </Routes>
+              <Footer />
+            </>
+          }
+        />
       </Routes>
-      <Footer />
     </BrowserRouter>
   );
-}
+};
 
 export default App;
